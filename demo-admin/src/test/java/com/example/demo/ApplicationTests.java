6e57b6b72185b366f9e56faeb586285f6b9141b5
10977 <1097795152@qq.com>
2017-11-30 13:57:14 +0800

    测试mapper代码

--- demo-admin/src/test/java/com/example/demo/ApplicationTests.java
+++ demo-admin/src/test/java/com/example/demo/ApplicationTests.java
@@ -1,17 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class ApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
     public void Test() {
 
+        List<User> userList = userMapper.findUserList();
 
+        User user = userList.get(0);
+
+        System.out.println(user.getAccount());
     }
 }
