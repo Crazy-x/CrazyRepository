2b52a8dd1acbd78a5d4474fd81030365f9edb0a5
10977 <1097795152@qq.com>
2017-11-30 11:33:00 +0800

    测试修改

--- demo-admin/src/test/java/com/example/demo/ApplicationTests.java
+++ demo-admin/src/test/java/com/example/demo/ApplicationTests.java
@@ -3,15 +3,11 @@ package com.example.demo;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+
 public class ApplicationTests {
 
     @Autowired
@@ -20,8 +16,10 @@ public class ApplicationTests {
     @Test
     public void contextLoads() {
 
-        List<User> userList = userMapper.findUserList();
-
-        System.out.println(userList.get(0).getAccount());
+//        List<User> userList = userMapper.findUserList();
+//
+//        System.out.println(userList.get(0).getAccount());
+        
+        System.out.println("111");
     }
 }
