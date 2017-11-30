22fa59fcdf2fca790a384f1eeafad058f2d986de
10977 <1097795152@qq.com>
2017-11-30 12:21:17 +0800

    修改测试类

--- demo-admin/src/test/java/com/example/demo/ApplicationTests.java
+++ demo-admin/src/test/java/com/example/demo/ApplicationTests.java
@@ -1,25 +1,17 @@
 package com.example.demo;
 
-import com.example.demo.entity.User;
-import com.example.demo.mapper.UserMapper;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
 public class ApplicationTests {
 
-    @Autowired
-    private UserMapper userMapper;
-
     @Test
-    public void contextLoads() {
+    public void Test() {
 
-//        List<User> userList = userMapper.findUserList();
-//
-//        System.out.println(userList.get(0).getAccount());
-        
-        System.out.println("111");
+
     }
 }
