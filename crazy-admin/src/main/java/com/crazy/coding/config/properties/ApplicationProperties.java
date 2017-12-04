992068df482434d7d2249d176c93a2f7fb1ca073
10977 <1097795152@qq.com>
2017-12-04 10:15:05 +0800

    增加注释

--- crazy-admin/src/main/java/com/crazy/coding/config/properties/ApplicationProperties.java
+++ crazy-admin/src/main/java/com/crazy/coding/config/properties/ApplicationProperties.java
@@ -3,13 +3,12 @@ package com.crazy.coding.config.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * 从*.yml配置文件中读取application下对应的属性，然后注入为bean
+ * 从*.yml配置文件中读取application下对应的属性
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = true)
 public class ApplicationProperties {
 
     private String basePackage;
-
 
     public String getBasePackage() {
         return basePackage;
