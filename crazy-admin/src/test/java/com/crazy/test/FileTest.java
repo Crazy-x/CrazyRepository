055ec9562204cacb68cd87bb243c3feba23f05b1
10977 <1097795152@qq.com>
2017-12-05 15:28:52 +0800

    test

---
+++ crazy-admin/src/test/java/com/crazy/test/FileTest.java
@@ -0,0 +1,94 @@
+package com.crazy.test;
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileTest {
+
+    private static List<String> filelist = new ArrayList<String>();
+
+    public static void main(String[] args) throws IOException {
+
+        String filePath = "F:\\IdeaProject\\eps\\eps-admin\\src";
+
+        getFiles(filePath);
+        for (String file : filelist) {
+            if (BufferedReaderAndBufferedWriter(file, "F:\\javadoc.txt")) {
+
+                System.out.println("文件写出成功");
+            } else {
+
+                System.out.println("文件写出失败");
+            }
+        }
+
+        System.out.println(filelist.size());
+    }
+
+    public static void getFiles(String filePath) {
+        File root = new File(filePath);
+        File[] files = root.listFiles();
+
+        for (File file : files) {
+
+            if (file.isDirectory()) {
+
+                getFiles(file.getAbsolutePath());
+            } else {
+
+                if (file.getAbsolutePath().endsWith(".java")) {
+
+                    filelist.add(file.getAbsolutePath());//将文件绝对路径放入到集合里面
+                }
+            }
+        }
+    }
+
+    public static Boolean BufferedReaderAndBufferedWriter(String inputFilePath, String outputFilePath) {
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        File file = null;//输出路径
+        try {
+            file = new File(outputFilePath);
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            //输出路径检查
+            bw = new BufferedWriter(new FileWriter(file, true));
+
+            //FileWriter里面两个参数，第二个 参数设置为true为不覆盖;
+            // 如果覆盖的话没有第二个参数，或者将第二个参数设置为false）；
+            //读取文件
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(inputFilePath), "UTF-8"));
+            String tempString = null;
+
+            while ((tempString = br.readLine()) != null) {
+                bw.write(tempString);//输出字符串
+                bw.newLine();//换行
+                bw.flush();
+            }
+            br.close();
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
+}
--- crazy-admin/src/test/java/com/crazy/test/Test.java
+++
@@ -1,9 +0,0 @@
-package com.crazy.test;
-
-
-public class Test {
-
-    public static void main(String[] args) {
-        Test test = new Test();
-    }
-}
