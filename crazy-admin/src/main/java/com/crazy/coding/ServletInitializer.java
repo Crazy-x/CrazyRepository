c94423cae46b73f9fc60dc049e616542acdc44ec
Administrator <1097795152@qq.com>
2018-03-26 15:15:40 +0800

    测试提交

--- crazy-admin/src/main/java/com/crazy/coding/ServletInitializer.java
+++ crazy-admin/src/main/java/com/crazy/coding/ServletInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
- * 可以通过war包启动
+ * 项目可以通过war包启动了
  */
 public class ServletInitializer extends SpringBootServletInitializer {
 
