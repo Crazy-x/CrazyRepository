a01ebf18c01b5fc069f23f7963d1fc1e52e6df54
Administrator <1097795152@qq.com>
2018-04-16 13:22:57 +0800

    测试mvc

--- crazy-admin/src/main/java/com/crazy/coding/api/TestApiController.java
+++ crazy-admin/src/main/java/com/crazy/coding/api/TestApiController.java
@@ -2,11 +2,7 @@ package com.crazy.coding.api;
 
 import com.crazy.coding.service.MsgProducerService;
 import com.crazy.coding.service.TestService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
